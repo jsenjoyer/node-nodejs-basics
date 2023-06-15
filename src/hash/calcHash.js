a7befcbb519e160bafd2d41ea6c12bae06166155
Uladzislau <uladzislau.plitok@leverx.com>
2023-06-15 23:28:49 +0200

    hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileData = await fs.readFile(
+      "./files/fileToCalculateHashFor.txt",
+      "utf-8"
+    );
+    const hash = crypto.createHash("sha256").update(fileData).digest("hex");
+    console.log(hash);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 await calculateHash();
