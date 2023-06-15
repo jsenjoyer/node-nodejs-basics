5e6e56eae753039b2b70e459aff8b1f1594a88d7
Uladzislau <uladzislau.plitok@leverx.com>
2023-06-15 22:04:48 +0200

    second part

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const ARGS = process.argv;
+
+  const finalstring = ARGS.filter((item) => item.includes("--"))
+    .map((arg) => {
+      return [
+        `${arg.substring(2, arg.length)} is ${
+          ARGS[ARGS.findIndex((item) => item === arg) + 1]
+        }`,
+      ];
+    })
+    .join(", ");
+  console.log(finalstring);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const objEntries = Object.entries(process.env);
+    const arrOfEntriesStartsWirhPrefix = objEntries.filter((item) => item[0].includes('RSS_'))
+    const finalString = arrOfEntriesStartsWirhPrefix.reduce((acc, item,index) => {
+        const [key,value]=item
+        const str = `${acc}; ${key}=${value}`
+        if(index===0){
+            return str.substring(2,str.length)
+        }
+        return str
+    }, '')
+    console.log(finalString)
 };
 
 parseEnv();
