3bc6c6e11526da2d28a79cd00de6f1407ae9ff9a
Uladzislau <uladzislau.plitok@leverx.com>
2023-06-16 01:06:31 +0200

    compress.js done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import {createWriteStream, createReadStream} from 'fs';
+import {createGzip} from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream('./files/fileToCompress.txt')
+    const writeStream = createWriteStream('archive.gz')
+    const gzipStream = createGzip();
+
+    readStream.pipe(gzipStream).pipe(writeStream)
 };
 
 await compress();
