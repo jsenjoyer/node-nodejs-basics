811bb9307e4f2de0ffab007aee2f71343a70c502
Uladzislau <uladzislau.plitok@leverx.com>
2023-06-16 01:12:08 +0200

    decompress.js done

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,11 @@
+import { createReadStream, createWriteStream } from 'fs';
+import {createGunzip} from 'zlib'
+//@Todo add __dirname
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream('./archive.gz')
+    const writeStream = createWriteStream('./files/file.txt','utf-8');
+    const unzipStream = createGunzip();
+    readStream.pipe(unzipStream).pipe(writeStream);
 };
 
 await decompress();
