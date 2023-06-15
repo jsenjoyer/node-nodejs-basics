7494e58975cb7a319ce70046f8f3b35a8efdc856
Uladzislau <uladzislau.plitok@leverx.com>
2023-06-16 00:35:38 +0200

    stream write done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import {createWriteStream} from 'fs'
+import {stdin} from 'process'
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const write = async () => {
-    // Write your code here 
+    const writebleStream = createWriteStream(`${__dirname}/files/fileToWrite.txt`, 'utf-8');
+    stdin.pipe(writebleStream);
 };
 
 await write();
