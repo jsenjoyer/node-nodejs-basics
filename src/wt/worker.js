79cef51856b292882b47f0e8d93a79b23c26101a
Uladzislau <uladzislau.plitok@leverx.com>
2023-06-16 02:27:22 +0200

    worker.js done

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,23 @@
-// n should be received from main thread
+import {isMainThread, Worker, parentPort} from 'worker_threads';
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const sendResult = (msg) => {
+    const res = nthFibonacci(msg);
+    parentPort.postMessage(res);
+};
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-};
+if (isMainThread) {
+    const worker = new Worker(__filename);
+    worker.on('message', (message) => {
+        console.log(message);
+    })
+    worker.postMessage(40)
+} else {
+    parentPort.on('message', (message) => {
+        sendResult(message)
+    })
+}
 
-sendResult();
